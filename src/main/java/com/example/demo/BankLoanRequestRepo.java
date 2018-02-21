f165557cd0a7c0f588205aa889c887989933f750
Vansi Dunna <vansi.dunna@ing.nl>
2018-02-21 16:50:16 +0100

    bank flow added

--- src/main/java/com/example/demo/BankLoanRequestRepo.java
+++ src/main/java/com/example/demo/BankLoanRequestRepo.java
@@ -12,24 +12,34 @@ import static com.googlecode.objectify.ObjectifyService.ofy;
 
 @Component
 public class BankLoanRequestRepo {
-	static {
+    static {
         ObjectifyService.register(BankLoanRequest.class);
     }
-	public void save(BankLoanRequest req) {
-		ObjectifyService.run(new VoidWork() {
+
+    public void save(BankLoanRequest req) {
+        ObjectifyService.run(new VoidWork() {
             @Override
             public void vrun() {
                 ofy().save().entity(req).now();
             }
         });
-	}
-	
-	public Optional<BankLoanRequest> get(Long id) {
-		return ObjectifyService.run(() -> Optional.ofNullable(ofy().load().key(Key.create(BankLoanRequest.class, id)).now()));
-	}
+    }
+
+    public Optional<BankLoanRequest> get(Long id) {
+        return ObjectifyService.run(() -> Optional.ofNullable(ofy().load().key(Key.create(BankLoanRequest.class, id)).now()));
+    }
 
     public List<BankLoanRequest> getAll() {
         return ObjectifyService.run(() -> ofy().load().type(BankLoanRequest.class).list());
     }
+
+    public List<BankLoanRequest> fetchByLoanRequestId(long loanRequestId) {
+        return ObjectifyService.run(() -> ofy().load().type(BankLoanRequest.class).filter("loanRequestId", loanRequestId).list());
+    }
+
+    public List<BankLoanRequest> fetchByBankId(String bankId) {
+        return ObjectifyService.run(() -> ofy().load().type(BankLoanRequest.class).filter("bankId", bankId).list());
+    }
+
 
 }
